09aa7dda76e843d5c407499152ee684ec80b1ea7
trick1z <tiker.zone.teen@gmail.com>
2025-08-19 18:08:05 +0700

    เพิ่ม Path user add form

--- ClientApp/src/app/modules/issue-inform/issue-inform-routing.module.ts
+++ ClientApp/src/app/modules/issue-inform/issue-inform-routing.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserRoute } from 'src/app/constants/routes.const';
 import { UserMainComponent } from './components/user-main/user-main.component';
+import { UserAddTaskComponent } from './components/user-add-task/user-add-task.component';
 
 const routes: Routes = [
   {
     path: UserRoute.UserForm,
     component: UserMainComponent
-  }
+  },
+  
+  {
+    path: UserRoute.UserAddForm,
+    component: UserAddTaskComponent
+  },
+
 
 ];
 
--- ClientApp/src/app/modules/issue-inform/issue-inform.module.ts
+++ ClientApp/src/app/modules/issue-inform/issue-inform.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { IssueInformRoutingModule } from './issue-inform-routing.module';
 import { UserMainComponent } from './components/user-main/user-main.component';
 import { SharedModule } from '../shared/shared.module';
+import { UserAddTaskComponent } from './components/user-add-task/user-add-task.component';
 
 
 @NgModule({
   declarations: [
-    UserMainComponent],
+    UserMainComponent,
+    UserAddTaskComponent],
   imports: [
     CommonModule,
     IssueInformRoutingModule,
