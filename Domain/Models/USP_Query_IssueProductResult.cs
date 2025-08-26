df244a8374aac8e74ee699a47ebdf08a1671ff88
trick1z <tiker.zone.teen@gmail.com>
2025-08-26 16:05:30 +0700

    แก้ Store ดึง Username ( Modifiedby )  มาด้วย

--- Domain/Models/USP_Query_IssueProductResult.cs
+++ Domain/Models/USP_Query_IssueProductResult.cs
@@ -13,6 +13,8 @@ namespace Domain.Models
         public string ProductName { get; set; }
         public DateTime CreatedTime { get; set; }
         public DateTime ModifiedTime { get; set; }
+        [StringLength(50)]
+        public string Username { get; set; }
         public int? TotalCount { get; set; }
     }
 }
